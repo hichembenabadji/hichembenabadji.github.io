8753666d21c7383f308d768bd01ec67d327cc31c
Hichem <benabadji.hichem@gmail.com>
2023-08-27 20:25:31 +0100

    minor changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,9 +41,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "7f13e21f21fdaa5364b8f83bb3438d2b",
 "icons/Icon-maskable-192.png": "c1544e9917ef0745a396d0e43e307ed9",
 "icons/Icon-maskable-512.png": "7f13e21f21fdaa5364b8f83bb3438d2b",
-"index.html": "c7335605ac4bdb6bc122c81ae0300efa",
-"/": "c7335605ac4bdb6bc122c81ae0300efa",
-"main.dart.js": "fb2091746788687c86490e70f2c1d402",
+"index.html": "883770866dd2a0b2a3e9acea8602a05c",
+"/": "883770866dd2a0b2a3e9acea8602a05c",
+"main.dart.js": "8788f741589a44fdd8fdf2cf54e9f92d",
 "manifest.json": "f71da04cb8925728a8a67660d00d30d8",
 "version.json": "f124181e58b12572810925130e6735b8"
 };
